e7f51995ac7d260242566fad66c10a8ac014b081
Alejandro-DP <alexdelacruz1888@gmail.com>
2021-12-06 18:34:29 -0600

    llenado de select BD

--- statusres.php
+++ statusres.php
@@ -33,11 +33,11 @@
                             <select class="custom-select" id="inputGroupSelect01">
                                 <option selected>Seleccione proyecto</option>
                                     <?php
-                                    // Realizamos la consulta para extraer los datos
+                                    
                                           
                                             $query = $conexion -> query ("SELECT * FROM projects");
                                             while ($valores = mysqli_fetch_array($query)) {
-                                    // En esta sección estamos llenando el select con datos extraidos de una base de datos.
+                                    
                                                 echo '<option value="'.$valores['id_project'].'">'.$valores['name_project'].'</option>';
                                             }
                                     ?>
